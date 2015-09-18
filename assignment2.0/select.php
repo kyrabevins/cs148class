71e805f0cc6c73109b3ed0da92ae99578d198e64
kyrabevins <kbevins@uvm.edu>
2015-09-17 22:08:11 -0400

    Changed sitemap and select

---
+++ assignment2.0/select.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+        <title>CS148 Assignment 2.0</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Kyra Bevins">
+        <meta name="description" content="This site is for Assignment 2.0 of CS148.">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <p>q01.   <a href="https://kbevins.w3.uvm.edu/cs148/assignment2.0/q01.php">SQL:</a>   SELECT pmkNetId FROM tblTeachers;</p>
+        
+</html>
--- sitemap.php
+++ sitemap.php
@@ -2,6 +2,6 @@
 
 
 <p><a href="assignment1.0/index.php">Assignment 1.0</a></p>
-<p><a href="assignment2.0/q01.php">Q01</a></p>
+<p><a href="assignment2.0/select.php">Assignment 2.0</a></p>
     
 
