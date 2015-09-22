35fcd21d0095da5b6f91e193ef536798d052ef58
kyrabevins <kbevins@uvm.edu>
2015-09-21 20:27:38 -0400

    final changes made for assignment2

--- sitemap.php
+++ sitemap.php
@@ -3,5 +3,18 @@
 
 <p><a href="assignment1.0/index.php">Assignment 1.0</a></p>
 <p><a href="assignment2.0/select.php">Assignment 2.0</a></p>
+<ul>
+    <li><a href="assignment2.0/q01.php">q01</a></li>
+    <li><a href="assignment2.0/q02.php">q02</a></li>
+    <li><a href="assignment2.0/q03.php">q03</a></li>
+    <li><a href="assignment2.0/q04.php">q04</a></li>
+    <li><a href="assignment2.0/q05.php">q05</a></li>
+    <li><a href="assignment2.0/q06.php">q06</a></li>
+    <li><a href="assignment2.0/q07.php">q07</a></li>
+    <li><a href="assignment2.0/q08.php">q08</a></li>
+    <li><a href="assignment2.0/q09.php">q09</a></li>
+    <li><a href="assignment2.0/q10.php">q10</a></li>
+    <li><a href="assignment2.0/q11.php">q11</a></li>
+</ul>
     
 
