b066d72b0cf97a4983721528dc4496226075983f
kyrabevins <kbevins@uvm.edu>
2015-10-05 22:26:53 -0400

    kdsja

--- sitemap.php
+++ sitemap.php
@@ -16,5 +16,15 @@
     <li><a href="assignment2.0/q10.php">q10</a></li>
     <li><a href="assignment2.0/q11.php">q11</a></li>
 </ul>
-    
 
+    <p><a href="assignment3.0/select.php">Assignment 3.0</a></p>
+
+<ul>
+    <li><a href="assignment3.0/q01.php">q01</a></li>
+    <li><a href="assignment3.0/q02.php">q02</a></li>
+    <li><a href="assignment3.0/q03.php">q03</a></li>
+    <li><a href="assignment3.0/q04.php">q04</a></li>
+    <li><a href="assignment3.0/q05.php">q05</a></li>
+    <li><a href="assignment3.0/q06.php">q06</a></li>
+    
+</ul>
